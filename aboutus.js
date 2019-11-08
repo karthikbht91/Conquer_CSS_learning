090f890136df8505593ecfd7365fb7d7471723d3
karthikbht91 <45008585+karthikbht91@users.noreply.github.com>
2019-11-08 11:47:02 +0530

    Added all the files

---
+++ aboutus.js
@@ -0,0 +1,20 @@
+//Code creates cards for the 'about us' page. Should extend it with JS components
+// function multiplyNode (count) {
+//     var card = document.getElementById('card-bgd');
+//     if(document.getElementsByClassName('card-background__div').length > 0){
+//         var elemArr = document.getElementsByClassName('card-background__div');
+//         elemArr.forEach(element => {
+//             element.remove();
+//         });
+//     }
+//     else{
+//     for(var i=0; i<=count-1; i++) {
+//         var cardContainer = document.getElementsByClassName('three-layout-structuring__div');
+//         let elem = document.createElement('div');
+//         elem.setAttribute('class', 'card-background__div');
+//         cardContainer[0].appendChild(elem);
+//        // card.cloneNode(true);
+
+//     }
+// }
+// }
